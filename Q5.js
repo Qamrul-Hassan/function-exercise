e9df190889aaaa15296850a808748ac48d3b52af
Qamrul-Hassan <sajal700@gmail.com>
2024-10-11 01:47:56 +0600

    massage

---
+++ Q5.js
@@ -0,0 +1,2 @@
+/* 5. Create a function that extracts a portion of a string using both `slice()` and `substring()` methods. 
+The start and end indices should be passed as arguments to the function.*/
---
+++ Q6.js
@@ -0,0 +1,8 @@
+/* 6. Write a function that takes two strings and concatenates them using `concat()`.
+ Also, demonstrate how to join an array 
+of strings into a single string using `join()`.*/
+
+function str (str1, str2){
+    return str1.concat(str2);
+}
+console.log(str("Qamrul ", "Hassan"));
