7e933c91d356c5f276651338830e4b78698e75e9
Qamrul-Hassan <sajal700@gmail.com>
2024-10-11 12:15:36 +0600

    massage

--- Q9.js
+++ Q9.js
@@ -1,2 +1,9 @@
 /* 9. Write a function that generates a random number between two given numbers using 
 `Math.random()` and ensures that the result is a whole number. */
+
+function getNandomNumber (min, max){
+    let randomNum = Math.random() * (max - min + 1)+min;
+    return Math.floor(randomNum);
+}
+console.log(getNandomNumber(1, 10));
+console.log(getNandomNumber(4,30));
